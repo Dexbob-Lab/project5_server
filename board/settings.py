f2c71af78f7b6d97c669db6b6475139f8e76446a
dexbob <mimacro@naver.com>
2024-10-31 16:44:27 +0900

    개발기간 환경정보 그대로 사용

--- board/settings.py
+++ board/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-bxarl*cz$yc*(us9^a3pa!*mg$84^^)0y_z-3!^b96&fgeu#+c'
-SECRET_KEY = env.getEnvValue("SECRET_KEY")
+SECRET_KEY = 'django-insecure-bxarl*cz$yc*(us9^a3pa!*mg$84^^)0y_z-3!^b96&fgeu#+c'
+# SECRET_KEY = env.getEnvValue("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
